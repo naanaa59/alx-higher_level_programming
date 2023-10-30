1659481ba1187a1be30effe041356c5dd64aa022
oumaima <oumaimanaanaapro@gmail.com>
2023-10-30 19:32:49 +0100

    task10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if there is a cycle in the list
+ *
+ * @list: the list to check
+ *
+ * Return: 0 if there is no cycle or 1 if there is a cycle
+ *
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast;
+	listint_t *slow;
+	listint_t *head = list->next;
+
+	if (head->next == NULL)
+		return (0);
+	fast = head->next;
+	slow = head;
+	while (fast != NULL && slow != NULL)
+	{
+		if (fast == slow)
+			return (1);
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	return (0);
+}
