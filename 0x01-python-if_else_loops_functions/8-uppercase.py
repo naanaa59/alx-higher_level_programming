acaab04731f5e7f329b391ece1376068eb3408af
oumaima <oumaimanaanaapro@gmail.com>
2023-10-31 21:55:33 +0100

    task8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     for i in range(1, len(str) + 1):
         asci_code = ord(str[i - 1])
-        if  96 < asci_code < 123:
-          upp  = chr(asci_code - 32)
+        if 96 < asci_code < 123:
+            upp = chr(asci_code - 32)
         else:
             upp = chr(asci_code)
         print("{}".format(upp), end='')
