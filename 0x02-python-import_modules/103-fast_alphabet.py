73a2181be2cd3251f2297d3bd39d123fffe5f60f
oumaima <oumaimanaanaapro@gmail.com>
2023-11-02 21:40:37 +0100

    task103 3 lines only

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 import string
-
-alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-         'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-print(''.join(alpha))
+print(''.join(string.ascii_uppercase))
