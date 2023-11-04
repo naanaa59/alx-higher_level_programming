0244b495b347fc150ff80edb34f74bce51605bba
oumaima <oumaimanaanaapro@gmail.com>
2023-11-04 17:29:08 +0100

    if logic

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,14 +8,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 2:
         a_first = tuple_a[0]
         a_second = tuple_a[1]
+    elif len(tuple_a) == 1:
+            a_first = tuple_a[0]
     if len(tuple_b) == 2:
         b_first = tuple_b[0]
         b_second = tuple_b[1]
-    else:
-        if len(tuple_a) == 1:
-            a_first = tuple_a[0]
-        if len(tuple_b) == 1:
-            b_first = tuple_a[0]
+    elif len(tuple_b) == 1:
+            b_first = tuple_b[0]
 
     new_tuple = a_first + b_first, a_second + b_second
     return new_tuple
