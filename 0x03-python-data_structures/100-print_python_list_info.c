53b987432f09009264d62bef5013e876ac4577bb
oumaima <oumaimanaanaapro@gmail.com>
2023-11-06 16:34:36 +0100

    typo

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -16,7 +16,7 @@ void print_python_list_info(PyObject *p)
 
 	size = PyList_Size(p);
 	printf("[*] Size of the Python List = %d\n", size);
-	printf("[*] Allocated = %lu\n", ((PyListOjbect *)p)->allocated);
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 
 	for (i = 0; i < size; i++)
 	{
