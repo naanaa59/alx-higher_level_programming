79582d2495c3516e03f3dc0d97f522a62012edbc
oumaima <oumaimanaanaapro@gmail.com>
2023-11-20 20:14:20 +0100

    t

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,9 +6,7 @@ def safe_print_integer_err(value):
     flag = True
     try:
         print("{:d}".format(value))
-    except ValueError as err:
+    except (ValueError, TypeError) as err:
         sys.stderr.write("Exception: {}\n".format(err))
-        flag = False
-    except TypeError:
         flag = False
     return flag
