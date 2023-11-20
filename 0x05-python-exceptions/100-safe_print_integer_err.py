0b1453e0d6067ad0121d88751cc26c8c30666c85
oumaima <oumaimanaanaapro@gmail.com>
2023-11-20 20:04:55 +0100

    task 100 adv new line added

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except ValueError as err:
-        sys.stderr.write("Exception: {}".format(err))
+        sys.stderr.write("Exception: {}\n".format(err))
         flag = False
     return flag
