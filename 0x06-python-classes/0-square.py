f375087478d56ce88648347aa1d0f4c6639325f0
oumaima <oumaimanaanaapro@gmail.com>
2023-11-21 18:26:00 +0100

    documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python3
+"""
+Module name: square_module
+
+This module defines a class named Square
+
+Class:
+    Square: defines a square
+
+Attributes:
+    None
+
+Usage:
+    n_square = Square()
+
+"""
+
+
 class Square:
     """
     This is an empty class named square
+
+    Attributes:
+        None
     """
+    def __init__(self):
+        """
+        Initialize a new instance of a square class
+
+        Args:
+            None
+
+        Returns:
+            None
+    """
+    pass
