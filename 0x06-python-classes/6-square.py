1cac2164cd8010b8f495673144ee5d3c03937835
oumaima <oumaimanaanaapro@gmail.com>
2023-11-21 22:45:24 +0100

    syntax checked

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,7 +46,7 @@ class Square:
             raise ValueError("size must be >= 0")
         if type(position) is not tuple or len(position) != 2\
            or type(position[0]) is not int or type(position[1]) is not\
-           int or position < (0, 0):
+           int or position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
 
         else:
@@ -132,7 +132,7 @@ class Square:
         """
         if type(value) is not tuple or len(value) != 2\
            or type(value[0]) is not int or type(value[1]) is not\
-           int or value < (0, 0):
+           int or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
