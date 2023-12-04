969ef24a97179c1b1021e704561d752c02a1b750
oumaima <oumaimanaanaapro@gmail.com>
2023-12-04 18:29:01 +0100

    task0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module is for inheretance project"""
+
+
+def lookup(obj):
+    """
+        This function returns the list of available
+        attributes and methods of an ojbect
+
+        Args: ojb: class
+    """
+    return dir(obj)
