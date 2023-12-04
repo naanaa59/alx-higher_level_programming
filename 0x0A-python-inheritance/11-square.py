d8c41aa8f685344441ba6f1b00727135cd4ae2c7
oumaima <oumaimanaanaapro@gmail.com>
2023-12-04 23:34:02 +0100

    11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    module defines square class
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+        class square that inherits from Rectangle
+    """
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return super().area()
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
