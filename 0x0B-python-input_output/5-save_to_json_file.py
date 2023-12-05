77fc88e4a79ff6e67412235db49826b32d258d65
oumaima <oumaimanaanaapro@gmail.com>
2023-12-05 22:14:13 +0100

    5 create empty file if it fails

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,6 +9,5 @@ def save_to_json_file(my_obj, filename):
     """
         writes an Object to a text file using JSON
     """
-    json_rep = json.dumps(my_obj)
     with open(filename, 'w') as f:
-        f.write(json_rep)
+        json.dump(my_obj, f)
