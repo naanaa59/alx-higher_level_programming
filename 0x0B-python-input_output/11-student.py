594c038b22526efdedce9c9a75171a89e6f9cfe8
oumaima <oumaimanaanaapro@gmail.com>
2023-12-06 00:51:37 +0100

    11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    This is a module defines a class named Student
+"""
+
+
+class Student():
+    """
+        Public instance attributes:
+            first_name
+            last_name
+            age
+        Method:
+            to_json
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all(type(att) is str for att in attrs):
+            return {attr: getattr(self, attr) for
+                    attr in attrs if hasattr(self, attr)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key in json:
+            setattr(self, key, json[key])
