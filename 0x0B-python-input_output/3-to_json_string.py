7ba89efa548bdb813e4cef06b7070adc5938b872
oumaima <oumaimanaanaapro@gmail.com>
2023-12-05 21:45:33 +0100

    3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -6,5 +6,8 @@ import json
 
 
 def to_json_string(my_obj):
+    """
+        eturns the JSON representation of an object (string)
+    """
     json_string = json.dumps(my_obj)
     return json_string
