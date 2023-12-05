32ad9936e7de6a78d6281ebf97475ea6c384a57d
oumaima <oumaimanaanaapro@gmail.com>
2023-12-05 21:09:16 +0100

    leave trailing whitespace

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,5 +11,5 @@ def read_file(filename=""):
     with open(filename, encoding="utf-8") as myFile:
         line = myFile.readline()
         while line:
-            print(line.strip())
+            print(line.strip('\n'))
             line = myFile.readline()
