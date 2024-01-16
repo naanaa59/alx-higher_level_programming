fb703b95412637f0af46587e423e4a5762f62235
oumaima <oumaimanaanaapro@gmail.com>
2024-01-17 00:21:24 +0100

    100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,4 @@
 -- converts hbtn_0c_0 database to utf8
 -- table: first_table to utf8
 -- field: name to utf8
-USE hbtn_0c_0
-ALTER TABLE 'first_table'
-CONVERT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
