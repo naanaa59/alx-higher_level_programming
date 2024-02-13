4a1556269869941cffaabcf6fff93a741d0e0d97
oumaima <oumaimanaanaapro@gmail.com>
2024-02-13 12:59:48 +0100

    task2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && h !== undefined && w !== undefined) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
