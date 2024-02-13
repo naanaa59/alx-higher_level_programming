8eb1c9a3b55913d73af130c85b4689133ed97525
oumaima <oumaimanaanaapro@gmail.com>
2024-02-13 21:59:53 +0100

    102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+
+const contentFileA = fs.readFileSync(process.argv[2], 'utf8');
+const contentFileB = fs.readFileSync(process.argv[3], 'utf8');
+
+fs.writeFileSync(process.argv[4], contentFileA + contentFileB, 'utf8');
