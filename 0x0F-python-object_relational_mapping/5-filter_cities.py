a6c77cc0d9763750f85fcd7daf004904f4698112
oumaima <oumaimanaanaapro@gmail.com>
2024-02-17 22:20:28 +0100

    5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    This script lists all cities from database hbtn_0e_4_usa
+    This script lists all cities from database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host='localhost', user=sys.argv[1], port=3306,
+                                 password=sys.argv[2], database=sys.argv[3])
+    cursor = connection.cursor()
+    state_name = sys.argv[4]
+    query = "SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') FROM cities\
+             INNER JOIN states ON cities.state_id = states.id\
+             WHERE states.name = %s"
+
+    cursor.execute(query, (state_name,))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(','.join(row))
+
+    cursor.close()
+    connection.close()
