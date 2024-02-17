525e4bdfb09fd9509949da66328f56b3605e3e81
oumaima <oumaimanaanaapro@gmail.com>
2024-02-17 23:36:51 +0100

    6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    This script contains the class definition of a State and an instance
+    Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+
+
+Base = declarative_base()
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                       .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column("id", Integer, primary_key=True, autoincrement=True,
+                nullable=False)
+    name = Column(String(128), nullable=False)
+
+
+Base.metadaba.create_all(engine)
