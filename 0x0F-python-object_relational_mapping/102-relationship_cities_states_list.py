41757073cfa4cf0f99cd8917d9e1fe30be203d53
oumaima <oumaimanaanaapro@gmail.com>
2024-02-18 19:56:54 +0100

    102

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    This script lists all the states from database hbtn_0e_100_usa
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, joinedload
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(user, pwd, db)
+    engine = create_engine(db_url)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).all()
+
+    for state in states:
+        for city in state.cities:
+            print(f"{city.id}: {city.name} -> {state.name}")
