13f612740faac1aa4a1a8ae11bbd66408d209d02
oumaima <oumaimanaanaapro@gmail.com>
2024-02-18 18:52:12 +0100

    101 tab

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -30,4 +30,4 @@ if __name__ == "__main__":
     for state in states:
         print(f"{state.id}: {state.name}")
         for city in state.cities:
-            print(f"    {city.id}: {city.name}")
+            print(f"\t{city.id}: {city.name}")
