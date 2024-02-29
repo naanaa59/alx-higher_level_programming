6e418478c6c08b8a6603077b2c78ae6d6f88ce88
oumaima <oumaimanaanaapro@gmail.com>
2024-02-29 23:15:21 +0100

    100 remove /n

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a request to that URL,  status code of the response
-curl -s -w '%{http_code}\n' -o /dev/null "$1"
+curl -s -w '%{http_code}' -o /dev/null "$1"
