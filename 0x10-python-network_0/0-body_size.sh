de8e48142153c9ba372a620ed205e6884defb179
oumaima <oumaimanaanaapro@gmail.com>
2024-02-29 21:38:10 +0100

    0 rm rcho

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
-echo$(curl -s -W '%{size_download}\n' -o /dev/null "$1")
+curl -s -W '%{size_download}\n' -o /dev/null "$1"
 
