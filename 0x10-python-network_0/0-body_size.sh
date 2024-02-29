e71fedaf9a1c9075d2ca8f5e0093f63b99f1d1e8
oumaima <oumaimanaanaapro@gmail.com>
2024-02-29 21:40:14 +0100

    0 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -W '%{size_download}\n' -o /dev/null "$1"
-
+# This script takes in a URL, sends a request to that URL, size of the body of the response
+curl -s -w '%{size_download}\n' -o /dev/null "$1"
