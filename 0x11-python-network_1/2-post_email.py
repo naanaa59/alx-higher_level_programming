7f058de2805f74df6f09fa8ada5dc395edd149d0
oumaima <oumaimanaanaapro@gmail.com>
2024-03-02 00:09:51 +0100

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,15 +4,15 @@
     to the passed URL
     with the email as a parameter, and displays the body of the response
 """
-import urllib
 import sys
+import urllib.parse
+import urllib.request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    data = {'email': email}
-    encoded_data = urllib.parse.urlencode(data).encode('utf-8')
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
