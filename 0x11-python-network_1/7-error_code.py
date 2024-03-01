b7e5d76d2032b4b219149513f549c829ade8e2b8
oumaima <oumaimanaanaapro@gmail.com>
2024-03-02 00:31:11 +0100

    7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    takes in a URL, sends a request to the URL and
+    displays the body of the response.
+"""
+import sys
+import requests
+
+
+import __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    if r.status_code >= 400:
+        print(f"Error code: {req.status_code}")
+    else:
+        print(r.text)
