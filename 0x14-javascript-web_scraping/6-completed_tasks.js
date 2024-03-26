62a9750b47397782c13836c27dae24dd2a1fcd69
oumaima <oumaimanaanaapro@gmail.com>
2024-03-26 22:41:58 +0000

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+const completeCount = {};
+
+request.get({ url, json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  if (response.statusCode === 200) {
+    body.forEach(task => {
+      if (!completeCount[task.userId]) {
+        completeCount[task.userId] = 0;
+      }
+      if (task.completed) {
+        completeCount[task.userId]++;
+      }
+    });
+    console.log(completeCount);
+  }
+});
