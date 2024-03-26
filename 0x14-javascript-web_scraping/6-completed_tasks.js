53cbbf83cf9691bd1ac2b6a7bad5448dd3c75c63
oumaima <oumaimanaanaapro@gmail.com>
2024-03-26 22:48:44 +0000

    6-completed_tasks.js checking completed first

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -13,11 +13,12 @@ request.get({ url, json: true }, (error, response, body) => {
   }
   if (response.statusCode === 200) {
     body.forEach(task => {
-      if (!completeCount[task.userId]) {
-        completeCount[task.userId] = 1;
-      }
       if (task.completed) {
-        completeCount[task.userId]++;
+        if (completeCount[task.userId] === undefined) {
+          completeCount[task.userId] = 1;
+        } else {
+          completeCount[task.userId]++;
+        }
       }
     });
     console.log(completeCount);
