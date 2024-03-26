24f0d65738ccf9c689ab61453af586775200bf90
oumaima <oumaimanaanaapro@gmail.com>
2024-03-26 21:31:59 +0000

    4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const idCharacter = 18;
+const urlCharacter = 'https://swapi-api.alx-tools.com/api/people/' + idCharacter + '/';
+let countFilms = 0;
+
+request.get({ url: apiUrl, json: true }, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  if (response.statusCode === 200) {
+    const films = body.results;
+    films.forEach(film => {
+      film.characters.forEach(character => {
+        if (character === urlCharacter) {
+          countFilms++;
+        }
+      });
+    });
+    console.log(countFilms);
+  }
+});
