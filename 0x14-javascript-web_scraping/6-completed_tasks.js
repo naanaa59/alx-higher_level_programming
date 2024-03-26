f089735c855016c165f8bdfb23995c9d29e1dd1d
oumaima <oumaimanaanaapro@gmail.com>
2024-03-26 22:44:04 +0000

    6-completed_tasks.js userId undefined handled

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -14,7 +14,7 @@ request.get({ url, json: true }, (error, response, body) => {
   if (response.statusCode === 200) {
     body.forEach(task => {
       if (!completeCount[task.userId]) {
-        completeCount[task.userId] = 0;
+        completeCount[task.userId] = 1;
       }
       if (task.completed) {
         completeCount[task.userId]++;
